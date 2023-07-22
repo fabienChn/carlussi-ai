7ff7968aeb41a47f267ee3f38d3cf30b0e286c10
Fabien Cohen <fabiencohen96@gmail.com>
2023-07-22 15:38:08 +0200

    secret key in dotenv

--- chatgpt.py
+++ chatgpt.py
@@ -1,13 +1,13 @@
-import os
-
-import constants
 from langchain.document_loaders import TextLoader
 from langchain.indexes import VectorstoreIndexCreator
 from langchain.chat_models import ChatOpenAI
 
-def transcript_to_answer(transcript):
-    os.environ["OPENAI_API_KEY"] = constants.APIKEY
+from dotenv import load_dotenv
 
+# Load environment variables from .env file
+load_dotenv()
+
+def transcript_to_answer(transcript):
     query = transcript
 
     loader = TextLoader('./data.txt')
