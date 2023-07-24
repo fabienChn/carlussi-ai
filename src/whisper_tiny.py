1e617a9e39758c1abd8a029491f82d241e82212b
Fabien Cohen <fabiencohen96@gmail.com>
2023-07-24 15:11:17 +0200

    getting rid of vosk and setting up whisper wip

---
+++ src/whisper_tiny.py
@@ -0,0 +1,25 @@
+import whisper
+import ssl
+
+# Ignore SSL certificate verification (not recommended for production use)
+ssl._create_default_https_context = ssl._create_default_https_context = ssl._create_unverified_context
+
+model = whisper.load_model("tiny")
+
+# load audio and pad/trim it to fit 30 seconds
+audio = whisper.load_audio("audio.mp3")
+audio = whisper.pad_or_trim(audio)
+
+# make log-Mel spectrogram and move to the same device as the model
+mel = whisper.log_mel_spectrogram(audio).to(model.device)
+
+# detect the spoken language
+_, probs = model.detect_language(mel)
+print(f"Detected language: {max(probs, key=probs.get)}")
+
+# decode the audio
+options = whisper.DecodingOptions()
+result = whisper.decode(model, mel, options)
+
+# print the recognized text
+print(result.text)
