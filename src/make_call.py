881fcb9616bc5437f5f700796bc7fa50a87a7fcc
Fabien Cohen <fabiencohen96@gmail.com>
2023-07-24 12:34:12 +0200

    dir structure

--- src/make_call.py
+++ src/make_call.py
@@ -9,9 +9,9 @@ auth_token = os.getenv('TWILIO_AUTH_TOKEN')
 client = Client(account_sid, auth_token)
 
 call = client.calls.create(
-                        url='http://demo.twilio.com/docs/voice.xml',
-                        to='+4917662693862',
-                        from_='+15737250186'
-                    )
+    url='http://demo.twilio.com/docs/voice.xml',
+    to='+4917662693862',
+    from_='+15737250186'
+)
 
 print(call.sid)
