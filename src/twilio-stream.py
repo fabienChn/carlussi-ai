e0176a64bd85cdd9266620d86e6944f550be9159
Fabien Cohen <fabiencohen96@gmail.com>
2023-08-03 23:39:14 +0200

    fixes and clean up

--- src/twilio-stream.py
+++ src/twilio-stream.py
@@ -1,71 +1,77 @@
-import asyncio
-import websockets
 from flask import Flask, request, Response
-from google.cloud import speech_v1p1beta1 as speech
-import threading
-import json
+from flask_socketio import SocketIO, emit
+from google.cloud import speech
 
 app = Flask(__name__)
-client = speech.SpeechClient()
+app.config['SECRET_KEY'] = 'your_secret_key_here'  # Replace with a random secret key
+socketio = SocketIO(app)
 
-request_config = {
+# Configure Transcription Request for German
+transcription_request = {
     "config": {
-        "encoding": speech.RecognitionConfig.AudioEncoding.MULAW,
-        "sample_rate_hertz": 8000,
-        "language_code": "de-DE",
+        "encoding": "MULAW",
+        "sampleRateHertz": 8000,
+        "languageCode": "de-DE",
         "model": "phone_call",
-        "use_enhanced": True,
+        "useEnhanced": True,
     },
-    "interim_results": True,
+    "interimResults": True,
 }
 
-async def handle_ws(websocket, path):
-    recognize_stream = None
-    async for message in websocket:
-        message = json.loads(message)
+# Handle Web Socket Connection
+@socketio.on('connect')
+def handle_connect():
+    print('New Connection Initiated')
+    recognizeStream = None
 
-        if message['event'] == 'connected':
-            print("A new call has connected.")
-            recognize_stream = client.streaming_recognize(request=request_config)
-        
-        elif message['event'] == 'start':
-            print(f"Starting Media Stream {message['streamSid']}")
+    @socketio.on('message')
+    def handle_message(msg):
+        event = msg.get('event')
+        if event == 'connected':
+            print('A new call has connected.')
 
-        elif message['event'] == 'media' and recognize_stream is not None:
-            recognize_stream.write(message['media']['payload'])
+            # Create Stream to the Google Speech to Text API
+            nonlocal recognizeStream
+            recognizeStream = speech.SpeechClient().streaming_recognize(transcription_request)
+            for data in recognizeStream:
+                transcript = data.results[0].alternatives[0].transcript
+                print(transcript)
 
-        elif message['event'] == 'stop':
-            print("Call Has Ended")
-            if recognize_stream:
-                recognize_stream.cancel()
+        elif event == 'start':
+            print('Starting Media Stream', msg.get('streamSid'))
 
-        await websocket.send("Response from server")
+        elif event == 'media':
+            # Check that recognizeStream is not None and not destroyed
+            if recognizeStream and not recognizeStream._stream.closed:
+                # Write Media Packets to the recognize stream
+                recognizeStream._stream.send(msg.get('media').get('payload'))
+            else:
+                print('Attempt to write to destroyed recognizeStream ignored')
 
-def start_ws_server():
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    ws_server = websockets.serve(handle_ws, "0.0.0.0", 8080)
-    loop.run_until_complete(ws_server)
-    loop.run_forever()
+        elif event == 'stop':
+            print('Call Has Ended')
+            if recognizeStream:
+                recognizeStream._stream.close()
 
-@app.route('/', methods=['GET'])
-def index():
-    return "Hello World"
+# Handle HTTP Request
+@app.route("/", methods=["GET", "POST"])
+def root():
+    print("request method is:")
+    print(request.method)
+    if request.method == "POST":
+        response = """
+            <Response>
+              <Start>
+                <Stream url="wss://{host}/"/>
+              </Start>
+              <Say>I will stream the next 60 seconds of audio through your websocket</Say>
+              <Pause length="60" />
+            </Response>
+        """.format(host=request.headers.get('Host'))
+        return Response(response, content_type='text/xml')
+    else:
+        return "Hello World"
 
-@app.route('/', methods=['POST'])
-def post():
-    host = request.headers['host']
-    response = f"""
-    <Response>
-        <Start>
-            <Stream url="wss://{host}/"/>
-        </Start>
-        <Say>I will stream the next 60 seconds of audio through your websocket</Say>
-        <Pause length="60" />
-    </Response>
-    """
-    return Response(response, content_type='text/xml')
-
-if __name__ == '__main__':
-    threading.Thread(target=start_ws_server).start()
-    app.run(port=8080)
+if __name__ == "__main__":
+    print("Listening at Port 8080")
+    socketio.run(app, host='0.0.0.0', port=8080)
